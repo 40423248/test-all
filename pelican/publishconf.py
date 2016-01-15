e14168e406d9cf08d3275c1a3fb35e9ad19e1cf8
40423248 <40423248@gm.nfu.edu.tw>
2016-01-15 16:03:35 +0800

    repatch

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '40423248'
-SITENAME = '2015FALL 40423248 CPB 作業'
+AUTHOR = '2015cpb '
+SITENAME = '2015FALL CPB 作業'
 SITEURL = ''
 
 PATH = 'content'
@@ -25,12 +25,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423248倉儲','http://40423248.github.io/cpbw3/'))
+LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'), ('Python', 'http://python.org/'),  ('2015作業主頁', '../../'), ('[github] 2015fallhw/2015fallcpb', 'https://github.com/2015fallhw/2015fallcpb/tree/gh-pages')
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
-          ('github', 'http://github.com/ametaireau'),)
+          ('github', 'http://github.com/ametaireau'))
           
 DEFAULT_PAGINATION = 10
 
@@ -49,7 +48,7 @@ DISQUS_SITENAME = "2015fall"
 #GOOGLE_ANALYTICS = ""
 
 # 必須絕對目錄或相對於設定檔案所在目錄
-PLUGIN_PATHS = ['./../../../plugin']
+PLUGIN_PATHS = ['./plugin']
 PLUGINS = ['liquid_tags.notebook']
 # 目錄設定相對於 reveal 下的 content 目錄
 NOTEBOOK_DIR = 'notebook'
